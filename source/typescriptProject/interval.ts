207293583748fa6f6bdb733552df0da20e6ec29f
Braeden-Rebel <b194r543@home.ku.edu>
2026-05-29 12:53:12 -0500

    Expanded intervals

--- source/typescriptProject/interval.ts
+++ source/typescriptProject/interval.ts
@@ -3,5 +3,20 @@ interface Interval
     leftBound: number | "inf" | "-inf",
     rightBound: number | "inf" | "-inf",
     leftContain: boolean,
-    rightContain: boolean
+    rightContain: boolean,
+    getLength() : number
 }
+
+interface BoundedInterval extends Interval
+{
+    leftBound: number,
+    rightBound: number,
+    getRandomWithin() : number
+}
+
+interface UnboundedInterval extends Interval
+{
+
+}
+
+export {Interval, BoundedInterval, UnboundedInterval}
