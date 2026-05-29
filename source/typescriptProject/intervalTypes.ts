7efaa8711293d703cdc78d5ddb7348a9aa986bdf
Braeden-Rebel <b194r543@home.ku.edu>
2026-05-29 14:33:41 -0500

    Unbounded Interval and Bounded infinite check

--- source/typescriptProject/intervalTypes.ts
+++ source/typescriptProject/intervalTypes.ts
@@ -7,6 +7,14 @@ class BoundedInterval implements IInterval
 {
     constructor(leftBound : number, leftIncluded : boolean, rightBound : number, rightIncluded : boolean)
     {
+        if (!isFinite(leftBound))
+        {
+            throw new TypeError("A bounded interval cannot accept infinity as a left bound");
+        }
+        else if (!isFinite(rightBound))
+        {
+            throw new TypeError("A bounded interval cannot accept infinity as a right bound");
+        }
         this.leftBound = leftBound;
         this.leftIncluded = leftIncluded;
         this.rightBound = rightBound;
@@ -18,7 +26,11 @@ class BoundedInterval implements IInterval
     rightIncluded: boolean;
 
     isWithin(x: number): boolean {
-        if (x == this.leftBound)
+        if (!isFinite(x))
+        {
+            return false;
+        }
+        else if (x == this.leftBound)
         {
             return this.leftIncluded;
         }
@@ -33,6 +45,44 @@ class BoundedInterval implements IInterval
     }
 }
 
-type Interval = BoundedInterval
+class UnboundedInterval implements IInterval
+{
+    constructor(leftBound : number, leftIncluded : boolean, rightBound : number, rightIncluded : boolean)
+    {
+        if (leftBound == Infinity)
+        {
+            
+        }
+        this.leftBound = leftBound;
+        this.leftIncluded = leftIncluded;
+        this.rightBound = rightBound;
+        this.rightIncluded = rightIncluded;
+    }
+    leftBound: number;
+    leftIncluded: boolean;
+    rightBound: number;
+    rightIncluded: boolean;
+
+    isWithin(x: number): boolean {
+        if (!isFinite(x))
+        {
+            return false;
+        }
+        else if (x == this.leftBound)
+        {
+            return this.leftIncluded;
+        }
+        else if (x == this.rightBound)
+        {
+            return this.rightIncluded;
+        }
+        else
+        {
+            return this.leftBound < x && x < this.rightBound;
+        }
+    }
+}
+
+type Interval = BoundedInterval | UnboundedInterval
 
 export {}
